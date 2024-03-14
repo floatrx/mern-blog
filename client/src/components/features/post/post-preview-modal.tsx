2e8ae4508e3ebb002dadb874d8487ca49f968096
Andrew floatrx <ifloatrx@gmail.com>
2024-03-14 04:57:50 +0200

    remove useless border

--- client/src/components/features/post/post-preview-modal.tsx
+++ client/src/components/features/post/post-preview-modal.tsx
@@ -33,7 +33,7 @@ export const PostPreviewModal = ({ post, onClose }: IProps) => {
         ref={ref}
         layoutId={post.id} // layoutId must sync with the card
         layoutRoot // prevents layout scaling
-        className="fixed top-[64px] inset-x-0 pointer-events-none max-h-[calc(100vh-64px)] overflow-y-auto border border-l-green-400-500"
+        className="fixed top-[64px] inset-x-0 pointer-events-none max-h-[calc(100vh-64px)] overflow-y-auto"
       >
         <PostContent onDismiss={onClose} post={post} />
         <hr className="my-10" />
