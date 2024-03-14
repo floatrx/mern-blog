afd00358a126f8ae85dc3d38ccbb0926a302420b
Andrew floatrx <ifloatrx@gmail.com>
2024-03-14 02:15:22 +0200

    fix scroll position

--- client/src/components/features/post/post-preview-modal.tsx
+++ client/src/components/features/post/post-preview-modal.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { motion } from 'framer-motion';
 
 import { PostContent } from '@/components/features/post/post-content';
@@ -9,24 +11,29 @@ interface IProps {
   onClose?: () => void;
 }
 
-export const PostPreviewModal = ({ post, onClose }: IProps) => (
-  <>
-    {/* Overlay */}
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      key="overlay"
-      className="fixed inset-0 bg-background/50 backdrop-blur-sm border-4 border-cyan"
-      onClick={() => onClose?.()}
-    />
-    {/* Preview modal */}
-    <motion.div
-      layoutId={post.id} // layoutId must sync with the card
-      layoutRoot // prevents layout scaling
-      className="absolute top-[72px] inset-x-0 flex items-start pointer-events-none"
-    >
-      <PostContent post={post} />
-    </motion.div>
-  </>
-);
+export const PostPreviewModal = ({ post, onClose }: IProps) => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
+  return (
+    <>
+      {/* Overlay */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        key="overlay"
+        className="fixed inset-0 bg-background/50 backdrop-blur-sm border-4 border-cyan"
+        onClick={() => onClose?.()}
+      />
+      {/* Preview modal */}
+      <motion.div
+        layoutId={post.id} // layoutId must sync with the card
+        layoutRoot // prevents layout scaling
+        className="absolute top-[72px] inset-x-0 flex items-start pointer-events-none"
+      >
+        <PostContent post={post} />
+      </motion.div>
+    </>
+  );
+};
