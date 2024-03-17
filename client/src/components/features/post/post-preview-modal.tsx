f0b75439d94e25607af2931276591818155c617b
Andrew floatrx <ifloatrx@gmail.com>
2024-03-17 18:46:48 +0200

    update post-preview-modal (ui)

--- client/src/components/features/post/post-preview-modal.tsx
+++ client/src/components/features/post/post-preview-modal.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 import { motion } from 'framer-motion';
 
 import { PostContent } from '@/components/features/post/post-content';
@@ -11,33 +9,25 @@ interface IProps {
   onClose?: () => void;
 }
 
-export const PostPreviewModal = ({ post, onClose }: IProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    // Scroll to ref top on mount
-    // ref.current?.scrollIntoView({ behavior: 'smooth' });
-  }, []);
-  return (
-    <>
-      {/* Overlay */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        key="overlay"
-        className="fixed inset-0 bg-background/50 sm:backdrop-blur-sm no-body-scroll"
-        onClick={() => onClose?.()}
-      />
-      {/* Preview modal */}
-      <motion.div
-        ref={ref}
-        layoutId={post.id} // layoutId must sync with the card
-        layoutRoot // prevents layout scaling
-        className="fixed top-[64px] inset-x-0 pointer-events-none max-h-[calc(100vh-64px)] overflow-y-auto invisible-scrollbar"
-      >
-        <PostContent onDismiss={onClose} post={post} />
-        <hr className="my-10" />
-      </motion.div>
-    </>
-  );
-};
+export const PostPreviewModal = ({ post, onClose }: IProps) => (
+  <>
+    {/* Overlay */}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      key="overlay"
+      className="fixed inset-0 bg-background/50 sm:backdrop-blur-sm no-body-scroll"
+      onClick={() => onClose?.()}
+    />
+    {/* Preview modal */}
+    <motion.div
+      layoutId={post.id} // layoutId must sync with the card
+      layoutRoot // prevents layout scaling
+      className="fixed top-[64px] inset-x-0 pointer-events-none max-h-[calc(100vh-64px)] overflow-y-auto invisible-scrollbar"
+    >
+      <PostContent onDismiss={onClose} post={post} />
+      <hr className="my-10" />
+    </motion.div>
+  </>
+);
