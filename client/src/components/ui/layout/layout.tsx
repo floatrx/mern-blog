05cb7cc90abb48beb4642aef507b5c7d6d329c3d
Andrew floatrx <ifloatrx@gmail.com>
2024-03-18 19:41:06 +0200

    fix link

--- client/src/components/ui/layout/layout.tsx
+++ client/src/components/ui/layout/layout.tsx
@@ -32,7 +32,7 @@ export const Layout = (props: PropsWithChildren) => {
         <div className="container p-4 text-xs text-gray-400">
           <p>
             © 2021 {SITE_NAME}. Test project for learning purposes.{' '}
-            <a href="https://github.com/floatrx/mongoose-express" target="_blank" rel="noopener" className="text-blue-500">
+            <a href="https://github.com/floatrx/mern-blog" target="_blank" rel="noopener" className="text-blue-500">
               GitHub.
             </a>{' '}
           </p>
